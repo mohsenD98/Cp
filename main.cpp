c5e8751a0c226bff06434b7fe0c72c43b775d4c2
Mohsen <mohsen_d98@yahoo.com>
2024-07-28 23:45:51 +0330

    kattis: IsItHalloween.com.

--- main.cpp
+++ main.cpp
@@ -78,8 +78,8 @@ int32_t main() {
 
 void solve() {
 
-    int a, b, c, n;
-    cin >> a >> b >> c >> n;
+    string s;
+    getline(cin, s);
 
-    if(a > 0 && b > 0 && c>0 && a+b+c >= n && n>2) cout << "YES"; else cout << "NO";
+    if(s == "OCT 31" || s == "DEC 25") cout << "yup"; else cout << "nope";
 }
